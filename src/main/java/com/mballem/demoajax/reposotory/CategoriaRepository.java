0e1a4ba317998f8f3693d96b4b669f5d4792a1dd
foaj <foaj@BRPC003901.gft.com>
2019-12-11 17:48:10 -0200

    Atualiando codigo

--- src/main/java/com/mballem/demoajax/reposotory/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mballem.demoajax.reposotory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mballem.demoajax.domain.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-}
